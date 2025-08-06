5c8903ef1b5a546077d5b56085b0bfb666fe1911
李鹏辉 <1553529191@qq.com>
2025-08-06 19:06:34 +0800

    修改合同下载问题

--- utils/desensitize.js
+++ utils/desensitize.js
@@ -4,7 +4,21 @@
  * @returns {string}
  */
 
-export function maskPhone(phone) {
+ function maskPhone(phone) {
     if (!phone) return '';
     return phone.replace(/^(\d{3})\d{4}(\d{4})$/, '$1****$2');
+}
+
+/**
+ * 判断路径是否是线上文件（http/https）
+ * @param {string} path 文件路径
+ * @returns {boolean}
+ */
+function isOnlineFile(path) {
+    return /^https?:\/\//.test(path);
+}
+
+export {
+    isOnlineFile,
+    maskPhone
 }
